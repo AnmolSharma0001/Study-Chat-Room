a99c2b986d9d5be8b9a12caa3162a7c9d6560ead
AnmolSharma0001 <anmol.as2002@gmail.com>
2023-07-06 02:15:58 +0530

    Extending crud and styling

--- base/views.py
+++ base/views.py
@@ -1,32 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
-# Create your views here.
-
-# def home(request):
-#     return HttpResponse('home page')
-
-# def room(request):
-#     return HttpResponse('room page')
-
-# rooms = [
-#     {'id': 1, 'name': 'new1'},
-#     {'id': 2, 'name': 'new2'},
-#     {'id': 3, 'name': 'new3'}
-# ]
+from django.db.models import Q
 
 
 def home(request):
-    rooms=Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q)
+                                )
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': rooms.count()}
     return render(request, 'home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    
-    context = {'room' : room}
+
+    context = {'room': room}
     return render(request, 'room.html', context)
 
 
@@ -39,5 +32,28 @@ def createRoom(request):
             form.save()
             return redirect('home')
 
-    context={'form' : form}
+    context = {'form': form}
     return render(request, "room_form.html", context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+
+    form = RoomForm(instance=room)
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'delete.html', {'obj': room})
