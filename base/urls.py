2f4676d60ec4a72a40207c3f388ec570205c818c
AnmolSharma0001 <anmol.as2002@gmail.com>
2024-07-19 03:52:07 +0530

    adding the user profile page

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path("room/<str:pk>/", views.room, name='room'),
+    path("profile/<str:pk>/", views.userProfile, name='user-profile'),
     path("create-room/", views.createRoom, name='create-room'),
     path("update-room/<str:pk>/", views.updateRoom, name='update-room'),
     path("delete-room/<str:pk>/", views.deleteRoom, name='delete-room'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path("logout/", views.logoutUser, name='logout'),
     path("register/", views.registerUser, name='register'),
     path("delete-message/<str:pk>/", views.deleteMessage, name='delete-message'),
+    
 ]
